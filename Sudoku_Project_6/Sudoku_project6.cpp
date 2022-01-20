6b264404becc23f1a66c1305e49d24b87ee369db
unknown <cvetan_toshev@abv.bg>
2022-01-20 15:55:06 +0200

    Last commit

--- Sudoku_Project_6/Sudoku_project6.cpp
+++ Sudoku_Project_6/Sudoku_project6.cpp
@@ -47,6 +47,222 @@ vector<vector<char>> specificLevelUnsolved(vector<vector<char>>& sudokoLevels, v
 	return sudokoUnsolved;
 }
 
+bool checkIfSolutionIsSeenOneTimeInTheRow(vector<vector<char>>& sudokoSolved, vector<vector<char>>& sudokoUnsolved, int row, char solution) {
+	char temp;
+	for (int i = 0; i < 9; i++) {
+		temp = sudokoUnsolved[row - 1][i];
+		if (temp == solution) {
+			return false;
+			break;
+		}
+	}
+	return true;
+}
+
+bool checkIfSolutionIsSeenOneTimeInTheColumn(vector<vector<char>>& sudokoSolved, vector<vector<char>>& sudokoUnsolved, int column, char solution) {
+	char temp;
+	for (int i = 0; i < 9; i++) {
+		temp = sudokoUnsolved[i][column - 1];
+		if (temp == solution) {
+			return false;
+			break;
+		}
+	}
+	return true;
+}
+
+bool checkIfSolutionIsSeenInABox(vector<vector<char>>& sudokoUnsolved, int column, int row, char solution) {
+	if ((row - 1) >= 0 && (row - 1) <= 2 && (column - 1) >= 0 && (column - 1) <= 2) {      // 1box
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (sudokoUnsolved[i][j] == solution) {
+					return false;
+					break;
+				}
+			}
+		}
+		return true;
+	}
+	else if ((row - 1) >= 0 && (row - 1) <= 2 && (column - 1) >= 3 && (column - 1) <= 5) {      // 2box
+		for (int i = 0; i < 3; i++) {
+			for (int j = 3; j < 6; j++) {
+				if (sudokoUnsolved[i][j] == solution) {
+					return false;
+					break;
+				}
+			}
+		}
+		return true;
+	}
+	else if ((row - 1) >= 0 && (row - 1) <= 2 && (column - 1) >= 6 && (column - 1) <= 8) {      // 3box
+		for (int i = 0; i < 3; i++) {
+			for (int j = 6; j < 9; j++) {
+				if (sudokoUnsolved[i][j] == solution) {
+					return false;
+					break;
+				}
+			}
+		}
+		return true;
+	}
+	else if ((row - 1) >= 3 && (row - 1) <= 5 && (column - 1) >= 0 && (column - 1) <= 2) {      // 4box
+		for (int i = 3; i < 6; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (sudokoUnsolved[i][j] == solution) {
+					return false;
+					break;
+				}
+			}
+		}
+		return true;
+	}
+	else if ((row - 1) >= 3 && (row - 1) <= 5 && (column - 1) >= 3 && (column - 1) <= 5) {      // 5box
+		for (int i = 3; i < 6; i++) {
+			for (int j = 3; j < 6; j++) {
+				if (sudokoUnsolved[i][j] == solution) {
+					return false;
+					break;
+				}
+			}
+		}
+		return true;
+	}
+	else if ((row - 1) >= 3 && (row - 1) <= 5 && (column - 1) >= 6 && (column - 1) <= 8) {      // 6box
+		for (int i = 3; i < 6; i++) {
+			for (int j = 6; j < 9; j++) {
+				if (sudokoUnsolved[i][j] == solution) {
+					return false;
+					break;
+				}
+			}
+		}
+		return true;
+	}
+	else if ((row - 1) >= 6 && (row - 1) <= 8 && (column - 1) >= 0 && (column - 1) <= 2) {      // 7box
+		for (int i = 6; i < 9; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (sudokoUnsolved[i][j] == solution) {
+					return false;
+					break;
+				}
+			}
+		}
+		return true;
+	}
+	else if ((row - 1) >= 6 && (row - 1) <= 8 && (column - 1) >= 3 && (column - 1) <= 5) {      // 8box
+		for (int i = 6; i < 9; i++) {
+			for (int j = 3; j < 6; j++) {
+				if (sudokoUnsolved[i][j] == solution) {
+					return false;
+					break;
+				}
+			}
+		}
+		return true;
+	}
+	else if ((row - 1) >= 6 && (row - 1) <= 8 && (column - 1) >= 6 && (column - 1) <= 8) {      // 9box
+		for (int i = 6; i < 9; i++) {
+			for (int j = 6; j < 9; j++) {
+				if (sudokoUnsolved[i][j] == solution) {
+					return false;
+					break;
+				}
+			}
+		}
+		return true;
+	}
+}
+
+bool checkingAnswers(vector<vector<char>>& sudokoUnsolved, vector<vector<char>>& sudokoSolved) {
+	int check = 0;
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (sudokoUnsolved[i][j] == sudokoSolved[i][j]) {
+				check++;
+			}
+			else {
+				return false;
+				break;
+			}
+		}
+	}
+	if (check == 81) {
+		return true;
+	}
+}
+
+int solvingSudoko(vector<vector<char>>& sudokoSolved, vector<vector<char>>& sudokoUnsolved, vector<vector<char>>& sudokoUnsolvedCheck) {
+
+	if (checkingAnswers(sudokoUnsolved, sudokoSolved)) {
+		cout << "C O N G R A T U L A T I O N S ! ! !" << endl << "Y O U  W O N  T H E  G A M E";
+		return 0;
+	}
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			cout << sudokoUnsolved[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	int check = 0;
+	int row = 0;
+	int column = 0;
+	char solution;
+	cout << endl;
+	cout << "Enter the coordinates of the cell you want to edit!" << endl;
+	cin >> row;
+	cin >> column;
+	cout << "Please, enter yor solution or type '0' to delete your solution!" << endl;
+	cin >> solution;
+	if (column < 1 || column > 9 || row < 1 || row > 9) {
+		cout << "Invalid coordinates!!" << endl;
+		cout << "Try again!" << endl;
+		cout << endl;
+		return solvingSudoko(sudokoSolved, sudokoUnsolved, sudokoUnsolvedCheck);
+	}
+	//cout << row << column << solution;
+	if (solution == '0') {
+		if (sudokoUnsolvedCheck[row - 1][column - 1] == '-' && sudokoUnsolved[row - 1][column - 1] != '-') {
+			sudokoUnsolved[row - 1][column - 1] = '-';
+		}
+		else if (sudokoUnsolvedCheck[row - 1][column - 1] == '-' && sudokoUnsolved[row - 1][column - 1] == '-') {
+			cout << "There is no solution to be deleted!" << endl;
+			cout << endl;
+			return solvingSudoko(sudokoSolved, sudokoUnsolved, sudokoUnsolvedCheck);
+		}
+		else {
+			cout << "You can not delete this number, because it is not your solution!" << endl;
+			cout << endl;
+		}
+
+	}
+	else {
+		if (sudokoUnsolvedCheck[row - 1][column - 1] != '-') {
+			cout << "You can not change this number, because it is not your solution!" << endl;
+			cout << endl;
+			return solvingSudoko(sudokoSolved, sudokoUnsolved, sudokoUnsolvedCheck);
+		}
+		else {
+			if (!checkIfSolutionIsSeenOneTimeInTheRow(sudokoSolved, sudokoUnsolved, row, solution)) {
+				cout << "There is already " << solution << " in that row!" << endl;
+				check++;
+			}
+			if (!checkIfSolutionIsSeenOneTimeInTheColumn(sudokoSolved, sudokoUnsolved, column, solution)) {
+				cout << "There is already " << solution << " in that column!" << endl;
+				check++;
+			}
+			if (!checkIfSolutionIsSeenInABox(sudokoUnsolved, column, row, solution)) {
+				cout << "There is already " << solution << " in the box!" << endl;
+				check++;
+			}
+			if (check == 0) {
+				sudokoUnsolved[row - 1][column - 1] = solution;
+			}
+		}
+	}
+	cout << endl;
+	return solvingSudoko(sudokoSolved, sudokoUnsolved, sudokoUnsolvedCheck);
+}
 
 int main() {
 	int randomSudokoLevel = levelGenerator();
@@ -86,6 +302,8 @@ int main() {
 	specificLevelSolved(sudokoLevels, sudokoSolved, level);
 	specificLevelUnsolved(sudokoLevels, sudokoUnsolved, level);
 	specificLevelUnsolved(sudokoLevels, sudokoUnsolvedCheck, level);
+
+	solvingSudoko(sudokoSolved, sudokoUnsolved, sudokoUnsolvedCheck);
 
 	return 0;
 }
